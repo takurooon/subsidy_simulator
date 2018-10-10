f0aaf7cb4521c94852ea4597150fe572ef5bf959
takurooon <takurokodama0726@gmail.com>
2018-10-10 15:03:20 +0900

    Add device_gem

--- app/controllers/simulators_controller.rb
+++ app/controllers/simulators_controller.rb
@@ -1,2 +1,20 @@
 class SimulatorsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :show]
+
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+
+  private
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'simulators#index'
+
   devise_for :users
 
   resources :user, only: [:new, :create, :show]
