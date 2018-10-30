99d307b2dadb56c9522f599da5df7cc35f01e202
takurooon <takurokodama0726@gmail.com>
2018-10-30 20:47:45 +0900

    refactoring simulators_controller

--- app/controllers/simulators_controller.rb
+++ app/controllers/simulators_controller.rb
@@ -9,118 +9,62 @@ class SimulatorsController < ApplicationController
 
   def under40; end
 
-  def result_under40
-    used_count = params[:used_count].to_i
-    collecting_eggs = params[:collecting_eggs].to_i
-    porting_eggs = params[:porting_eggs].to_i
-    pay = params[:pay].to_i
-    @pay = pay
-    total = collecting_eggs + porting_eggs
-    upper_limit = 6
-    unit_price = 75_000
-    special_price = 150_000
-    subsidy = total * unit_price
-    limit = upper_limit - used_count
-    @remain = limit - 1
-    @actual_cost = pay - subsidy
-    date = Date.today
-    @date = date.financial_year
-
-    if @pay <= 0
-      redirect_to simulators_under40_path, alert: "支払った医療費が無い場合は助成対象にはなりません。またマイナスの入力は出来ません。"
-      return
-    end
-
-    if @remain < 0
-      @remain = 0
-    end
-
-    if used_count >= upper_limit
-      render "simulators/result_nomatch"
-    elsif used_count < upper_limit && used_count == 0 && total != 0
-      @result = subsidy + special_price
-      @actual_cost = pay - subsidy - special_price
-      session[:result] = @result
-      session[:actual_cost] = @actual_cost
-      session[:remain] = @remain
-      session[:date] = @date
-      if @actual_cost < 0
-        @actual_cost = 0
-      end
-      gon.data = [@result, @actual_cost]
-      render "simulators/result"
-    else
-      @result = subsidy
-      session[:result] = @result
-      session[:actual_cost] = @actual_cost
-      session[:remain] = @remain
-      session[:date] = @date
-      if @actual_cost < 0
-        @actual_cost = 0
-      end
-      gon.data = [@result, @actual_cost]
-      render "simulators/result"
-    end
-  end
-
   def under43; end
 
-  def result_under43
+  def result_under40
+    max_treat = 6
     used_count = params[:used_count].to_i
     collecting_eggs = params[:collecting_eggs].to_i
     porting_eggs = params[:porting_eggs].to_i
     pay = params[:pay].to_i
-    @pay = pay
     total = collecting_eggs + porting_eggs
-    upper_limit = 3
-    unit_price = 75_000
-    special_price = 150_000
-    subsidy = total * unit_price
-    limit = upper_limit - used_count
-    @remain = limit - 1
-    @actual_cost = pay - subsidy
-    date = Date.today
-    @date = date.financial_year
+
+    if used_count >= max_treat || total == 0
+      render "simulators/result_nomatch"
+      return
+    end
+
+    @result, @actual_cost, @remain_subsidy, @pay, @result_pay = calc_result(used_count, collecting_eggs, porting_eggs, pay, max_treat, total)
 
     if @pay <= 0
       redirect_to simulators_under40_path, alert: "支払った医療費が無い場合は助成対象にはなりません。またマイナスの入力は出来ません。"
       return
     end
 
-    if @remain < 0
-      @remain = 0
+    @date = Date.today.financial_year
+    save_session(@result, @actual_cost, @remain_subsidy, @date)
+    gon.data = [@result, @actual_cost]
+    render "simulators/result"
+  end
+
+  def result_under43
+    max_treat = 3
+    used_count = params[:used_count].to_i
+    collecting_eggs = params[:collecting_eggs].to_i
+    porting_eggs = params[:porting_eggs].to_i
+    pay = params[:pay].to_i
+    total = collecting_eggs + porting_eggs
+
+    if used_count >= max_treat || total == 0
+      render "simulators/result_nomatch"
+      return
     end
 
-    if used_count >= upper_limit
-      render "simulators/result_nomatch"
-    elsif used_count < upper_limit && used_count == 0 && total != 0
-      @result = subsidy + special_price
-      @actual_cost = pay - subsidy - special_price
-      session[:result] = @result
-      session[:actual_cost] = @actual_cost
-      session[:remain] = @remain
-      session[:date] = @date
-      if @actual_cost < 0
-        @actual_cost = 0
-      end
-      gon.data = [@result, @actual_cost]
-      render "simulators/result"
-    else
-      @result = subsidy
-      session[:result] = @result
-      session[:actual_cost] = @actual_cost
-      session[:remain] = @remain
-      session[:date] = @date
-      if @actual_cost < 0
-        @actual_cost = 0
-      end
-      gon.data = [@result, @actual_cost]
-      render "simulators/result"
+    @result, @actual_cost, @remain_subsidy, @pay, @result_pay = calc_result(used_count, collecting_eggs, porting_eggs, pay, max_treat, total)
+
+    if @pay <= 0
+      redirect_to simulators_under43_path, alert: "支払った医療費が無い場合は助成対象にはなりません。またマイナスの入力は出来ません。"
+      return
     end
+
+    @date = Date.today.financial_year
+    save_session(@result, @actual_cost, @remain_subsidy, @date)
+    gon.data = [@result, @actual_cost]
+    render "simulators/result"
   end
 
   def result_nomatch
-    render 'simulators/result_nomatch'
+    render "simulators/result_nomatch"
   end
 
   def send_mail
@@ -128,4 +72,20 @@ class SimulatorsController < ApplicationController
     render "simulators/send_mail"
   end
 
+  private
+
+  def calc_result(used_count, collecting_eggs, porting_eggs, pay, max_treat, total)
+    result = total * 75_000
+    result += 150_000 if used_count == 0 && total != 0
+    actual_cost = pay - result
+    remain_subsidy = max_treat - 1
+    return result, actual_cost, remain_subsidy, pay
+  end
+
+  def save_session(result, actual_cost, remain_subsidy, date)
+    session[:result] = result
+    session[:actual_cost] = actual_cost
+    session[:remain_subsidy] = remain_subsidy
+    session[:date] = date
+  end
 end
--- app/mailers/result_mailer.rb
+++ app/mailers/result_mailer.rb
@@ -1,9 +1,9 @@
 class ResultMailer < ApplicationMailer
 
-  def send_result(user, result, actual_cost, date, remain)
+  def send_result(user, result, actual_cost, date, remain_subsidy)
     @result = result
     @actual_cost = actual_cost
-    @remain = remain
+    @remain_subsidy = remain_subsidy
     @date = date
     mail to: user.email, subject: "診断結果"
   end
